14d4986132faefa53a92d5b4d7112e06aed6a91e
AKBagheL <74250031+AK-Baghel@users.noreply.github.com>
2022-12-10 11:35:56 +0530

    Time: 14 ms (46.71%), Space: 54.6 MB (44.29%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+       for (int i = 0, j = s.length() - 1; i < j; i++,j--)
+      if (s.charAt(i) != s.charAt(j))
+        return isPalindrome(s, i+1, j) || isPalindrome(s, i, j-1);
+
+    return true;
+  }
+  private boolean isPalindrome(String s, int i, int j) {
+    for (; i < j; i++,j--)
+      if (s.charAt(i) != s.charAt(j)) return false;
+
+    return true;
+  }
+}
