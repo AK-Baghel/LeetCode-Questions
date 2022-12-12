655e3518da880bfdc19406103851254792b50a08
AKBagheL <74250031+AK-Baghel@users.noreply.github.com>
2022-12-12 10:03:39 +0530

    Time: 13 ms (19.34%), Space: 42.3 MB (42.46%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -2,38 +2,29 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
         String commonPrefix = "";
 		
-		// Check if array is empty.
-        if(strs.length==0) return commonPrefix;
+        if(strs.length==0)
+             return commonPrefix;
         
-		// Get the smallest string in the Array.
-        String smallest = getSmallestString(strs);
+        String smallest = strs[0];
+        for(int i=1;i<strs.length;i++){
+            if(strs[i].length()<smallest.length())
+                smallest = strs[i];
+        }        
 		
-		// Run loop for each char
         for(int i=0; i<smallest.length(); i++){
-            boolean matchFlag = true;
-			// Check char in all strings
+            boolean flag=true;
             for(String str: strs){
                 if(str.charAt(i)!=smallest.charAt(i)){
-                    matchFlag=false;
+                    flag=false;
                     break;
                 }
             }
-            if(!matchFlag){
-                break;
-            }
-            commonPrefix+=smallest.charAt(i);
+            if(flag)
+                commonPrefix+=smallest.charAt(i);
+            else
+                return commonPrefix;
         }
         return commonPrefix;
     }
-    
-	//Method to return smallest string from the array.
-    public String getSmallestString(String[] strs){
-        String smallest = strs[0];
-        for(int i=1;i<strs.length;i++){
-            if(strs[i].length()<smallest.length()){
-                smallest = strs[i];
-            }
-        }
-        return smallest;
-    }
 }
+
