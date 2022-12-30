f4f2e3d6e7f9add06f30d80f824b05788c903382
AKBagheL <74250031+AK-Baghel@users.noreply.github.com>
2022-12-30 12:41:52 +0530

    Time: 1 ms (100.00%), Space: 42.5 MB (86.35%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -4,10 +4,11 @@ class Solution {
         for(int i=0;i<indices.length;i++){
             arr[indices[i]]=s.charAt(i);
         }
-        String sb="";
-        for(char i:arr){
-            sb+=i;
-        }
-        return sb;
+        // String sb="";
+        // for(char i:arr){
+        //     sb+=i;
+        // }
+        // return sb;
+        return new String(arr);
     }
 }
