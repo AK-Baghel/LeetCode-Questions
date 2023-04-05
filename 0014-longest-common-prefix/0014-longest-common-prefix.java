37cca4c5d19c1df925de5acb831c17a2e478db74
Ankit Kumar <74250031+AK-Baghel@users.noreply.github.com>
2023-04-05 09:06:25 +0530

    Time: 1 ms (75.22%), Space: 40.9 MB (37.85%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,29 +1,42 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        String commonPrefix = "";
-        if(strs.length==0)
-             return commonPrefix;
         
-        String smallest = strs[0];
-        for(int i=1;i<strs.length;i++){
-            if(strs[i].length()<smallest.length())
-                smallest = strs[i];
-        }        
-		
-        for(int i=0; i<smallest.length(); i++){
-            boolean flag=true;
-            for(String str: strs){
-                if(str.charAt(i)!=smallest.charAt(i)){
-                    flag=false;
-                    break;
-                }
+        Arrays.sort(strs);
+        int i;
+
+        for(i = 0; i < strs[0].length(); i++){
+            if(strs[0].charAt(i) != strs[strs.length - 1].charAt(i)){
+                break;
             }
-            if(flag)
-                commonPrefix+=smallest.charAt(i);
-            else
-                return commonPrefix;
         }
-        return commonPrefix;
+
+        return strs[0].substring(0, i); 
+        
+        
+//         String commonPrefix = "";
+//         if(strs.length==0)
+//              return commonPrefix;
+        
+//         String smallest = strs[0];
+//         for(int i=1;i<strs.length;i++){
+//             if(strs[i].length()<smallest.length())
+//                 smallest = strs[i];
+//         }        
+		
+//         for(int i=0; i<smallest.length(); i++){
+//             boolean flag=true;
+//             for(String str: strs){
+//                 if(str.charAt(i)!=smallest.charAt(i)){
+//                     flag=false;
+//                     break;
+//                 }
+//             }
+//             if(flag)
+//                 commonPrefix+=smallest.charAt(i);
+//             else
+//                 return commonPrefix;
+//         }
+//         return commonPrefix;
     }
 }
 
