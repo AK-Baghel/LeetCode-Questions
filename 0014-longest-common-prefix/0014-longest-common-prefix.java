6760655b309b66fe6d8dc8c73395ef201d368c67
Ankit Kumar <74250031+AK-Baghel@users.noreply.github.com>
2023-04-06 20:15:33 +0530

    Time: 1 ms (75.24%), Space: 40.6 MB (58.77%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -3,13 +3,11 @@ class Solution {
         
         Arrays.sort(strs);
         int i;
-
         for(i = 0; i < strs[0].length(); i++){
             if(strs[0].charAt(i) != strs[strs.length - 1].charAt(i)){
                 break;
             }
         }
-
         return strs[0].substring(0, i); 
         
         
